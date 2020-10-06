df5f45ed20a23a01746524375f5f734264af47d2
Arthur Busser <arthur.busser@gmail.com>
2020-10-06 19:06:11 +0200

    fix(version): use original values

--- version/version.go
+++ version/version.go
@@ -8,11 +8,11 @@ import (
 
 var (
 	// Version is Clamp's version.
-	Version = "unknown version"
+	Version = "dev"
 	// Commit is the hash of the commit used to build Clamp.
-	Commit = "unknown commit"
+	Commit = "n/a"
 	// Date is when Clamp was built.
-	Date = "unknown build date"
+	Date = "n/a"
 )
 
 // Print writes Clamp's version information to standard output.
