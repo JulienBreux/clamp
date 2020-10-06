7751aff4d0e9b5cae31591b83aebda4146e34d2f
Arthur Busser <arthur.busser@gmail.com>
2020-10-06 19:22:09 +0200

    fix(functions): remove unnecessary block nesting

--- functions/required.go
+++ functions/required.go
@@ -5,10 +5,9 @@ import "errors"
 func required(msg string, val interface{}) (interface{}, error) {
 	if val == nil {
 		return val, errors.New(msg)
-	} else if _, ok := val.(string); ok {
-		if val == "" {
-			return val, errors.New(msg)
-		}
+	}
+	if _, ok := val.(string); ok && val == "" {
+		return val, errors.New(msg)
 	}
 	return val, nil
 }
