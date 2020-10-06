69ed7ced0eb64fa0c6dc93c39a7b297209a87f83
Arthur Busser <arthur.busser@gmail.com>
2020-10-06 19:23:17 +0200

    clean(functions): clean up FuncMap instantiation

--- functions/map.go
+++ functions/map.go
@@ -4,9 +4,7 @@ import "text/template"
 
 // Map returns a map of functions to use with Go's standard template package.
 func Map() template.FuncMap {
-	fm := make(template.FuncMap)
-
-	fm["required"] = required
-
-	return fm
+	return template.FuncMap{
+		"required": required,
+	}
 }
